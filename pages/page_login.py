bc982124bd2d652f36e2027effb350e05f8342e8
WhooGeek <rlarjswn0303@naver.com>
2026-01-06 15:39:31 +0900

    FIX: 테스트 환경 안정화 수정

--- pages/page_login.py
+++ pages/page_login.py
@@ -17,7 +17,7 @@ class LoginPage:
         # locator.press_sequentially 사용 (요소에 직접 입력)
         # delay를 100ms로 늘려 CI의 낮은 CPU 성능을 고려
         self.email.click() # 클릭 유지
-        self.email.press_sequentially(username, delay=100)
+        self.email.press_sequentially(username, delay=200)
         
         # 'Tab' 키로 자연스러운 Blur 유도
         self.email.press("Tab") 
@@ -25,13 +25,14 @@ class LoginPage:
 
         # 비밀번호 입력
         self.password.click()
-        self.password.press_sequentially(password, delay=100)
+        self.password.press_sequentially(password, delay=200)
         self.password.press("Tab") # 탭 눌러서 포커스 해제
+        self.page.wait_for_timeout(1000)
         
         # 버튼 활성화 'Assertions' (Retry 로직 내장)
         # enabled 될 때까지 Playwright가 계속 찔러봅니다.
-        expect(self.submit_btn).to_be_enabled(timeout=15000) # CI 고려 15초로 넉넉하게
+        expect(self.submit_btn).to_be_enabled(timeout=30000) # CI 고려 15초로 넉넉하게
         self.submit_btn.click()
 
         # 로그인 성공 확인
-        expect(self.user_avatar).to_be_visible(timeout=10000)
+        expect(self.user_avatar).to_be_visible(timeout=30000)
